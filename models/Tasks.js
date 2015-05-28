73f9f9fcba8fc81dccf9b55448ff47ea146230b8
Annapurna Singh <annapurna.singh@gmail.com>
2015-05-28 11:08:12 -0500

    more MEAN

---
+++ models/Tasks.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var TaskSchema= new mongoose.Schema({
+  name: String,
+  completed: Boolean,
+  description: String,
+  updated_at: { type: Date, "default": Date.now }
+
+});
+
+//export to my node app!
+module.export = mongoose.model('Task', TaskSchema);
